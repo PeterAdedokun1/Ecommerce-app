6585d2d02e0c538240dc1893865da9ece325e97e
peterAdedokun1 <peteradedokun436@gmail.com>
2023-09-02 20:53:45 +0100

    home slider section

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,11 +7,57 @@ import {
   ScrollView,
   Pressable,
   TextInput,
+  Image,
 } from "react-native";
 import React from "react";
-
+//icons
 import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { SliderBox } from "react-native-image-slider-box";
 const HomeScreen = () => {
+  const list = [
+    {
+      id: "0",
+      image: "https://m.media-amazon.com/images/I/41EcYoIZhIL._AC_SY400_.jpg",
+      name: "Home",
+    },
+    {
+      id: "1",
+      image:
+        "https://m.media-amazon.com/images/G/31/img20/Events/Jup21dealsgrid/blockbuster.jpg",
+      name: "Deals",
+    },
+    {
+      id: "3",
+      image:
+        "https://images-eu.ssl-images-amazon.com/images/I/31dXEvtxidL._AC_SX368_.jpg",
+      name: "Electronics",
+    },
+    {
+      id: "4",
+      image:
+        "https://m.media-amazon.com/images/G/31/img20/Events/Jup21dealsgrid/All_Icons_Template_1_icons_01.jpg",
+      name: "Mobiles",
+    },
+    {
+      id: "5",
+      image:
+        "https://m.media-amazon.com/images/G/31/img20/Events/Jup21dealsgrid/music.jpg",
+      name: "Music",
+    },
+    {
+      id: "6",
+      image: "https://m.media-amazon.com/images/I/51dZ19miAbL._AC_SY350_.jpg",
+      name: "Fashion",
+    },
+  ];
+  const images = [
+    "https://img.etimg.com/thumb/msid-93051525,width-1070,height-580,imgsize-2243475,overlay-economictimes/photo.jpg",
+    "https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/PD23/Launches/Updated_ingress1242x550_3.gif",
+    "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Books/BB/JULY/1242x550_Header-BB-Jul23.jpg",
+  ];
   return (
     <SafeAreaView
       style={{
@@ -21,7 +67,14 @@ const HomeScreen = () => {
       }}
     >
       <ScrollView>
-        <View style={{backgroundColor: "#00ced1", padding: 10, flexDirection: "row", alignItems: "center"}}>
+        <View
+          style={{
+            backgroundColor: "#00ced1",
+            padding: 10,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
           <Pressable
             style={{
               flexDirection: "row",
@@ -34,10 +87,72 @@ const HomeScreen = () => {
               flex: 1,
             }}
           >
-            <AntDesign name="search1" size={22} color="black" style={{ paddingLeft: 10 }} />
+            <AntDesign
+              name="search1"
+              size={22}
+              color="black"
+              style={{ paddingLeft: 10 }}
+            />
             <TextInput placeholder="Search amazon" />
           </Pressable>
+          <Feather name="mic" size={24} color="black" />
         </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            padding: 10,
+            backgroundColor: "#AFEEEE",
+            marginBottom: "10px",
+          }}
+        >
+          <Ionicons name="location-outline" size={24} color="black" />
+          <Pressable>
+            <Text style={{ fontSize: 13, fontWeight: "500" }}>
+              Deliver to abule oja - yaba lagos
+            </Text>
+          </Pressable>
+
+          <MaterialIcons name="keyboard-arrow-down" size={24} color="black" />
+        </View>
+
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {list.map((item, index) => (
+            <Pressable
+              key={index}
+              style={{
+                margin: 10,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                style={{ width: 50, height: 50, resizeMode: "contain" }}
+                source={{ uri: item.image }}    
+              />
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 12,
+                  fontWeight: "500",
+                  marginTop: 5,
+                }}
+              >
+                {item?.name}
+              </Text>
+            </Pressable>
+          ))}
+        </ScrollView>
+        <SliderBox
+          images={images}
+          autoplay
+          circleLoop
+          dotColor={"#13274F"}
+          inactiveDotColor={"#9084ae"}
+          ImageComponentStyle={{ width: "100%" }}
+        />
       </ScrollView>
     </SafeAreaView>
   );
