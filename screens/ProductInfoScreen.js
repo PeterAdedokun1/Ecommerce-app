96c583c4c86fcb60c8ce3238bc022ff0003a6032
peterAdedokun1 <peteradedokun436@gmail.com>
2023-09-03 19:08:52 +0100

    description

--- screens/ProductInfoScreen.js
+++ screens/ProductInfoScreen.js
@@ -4,21 +4,22 @@ import {
   View,
   Platform,
   ScrollView,
-    SafeAreaView,
-    TextInput,
+  SafeAreaView,
+  TextInput,
   Pressable,
   ImageBackground,
-  Dimensions
+  Dimensions,
 } from "react-native";
 import React from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
-import { useRoute , useNavigation} from "@react-navigation/native";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 const ProductInfoScreen = () => {
-    const route = useRoute();
-    const navigation = useNavigation()
-    const { width } = Dimensions.get("window");
-    const height = (width * 100) / 100; 
+  const route = useRoute();
+  const navigation = useNavigation();
+  const { width } = Dimensions.get("window");
+  const height = (width * 100) / 100;
   return (
     <ScrollView
       style={{ marginTop: 45, flex: 1, backgroundColor: "white" }}
@@ -55,12 +56,101 @@ const ProductInfoScreen = () => {
         <Feather name="mic" size={24} color="black" />
       </View>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-              {route.params.carouselImages.map((item, index) => (
-                  <ImageBackground style={{width, height, marginTop:25, resizeMode: "contain"}} source={{uri: item}} key={index}>
-                      <View></View>
-            </ImageBackground>
+        {route.params.carouselImages.map((item, index) => (
+          <ImageBackground
+            style={{ width, height, marginTop: 25, resizeMode: "contain" }}
+            source={{ uri: item }}
+            key={index}
+          >
+            <View
+              style={{
+                padding: 20,
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <View
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 20,
+                  backgroundColor: "#C60C30",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "row",
+                }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    textAlign: "center",
+                    fontWeight: "600",
+                    fontSize: 12,
+                  }}
+                >
+                  20% offer
+                </Text>
+              </View>
+              <View
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 20,
+                  backgroundColor: "#E0E0E0",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "row",
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="share-variant"
+                  size={24}
+                  color="black"
+                />
+              </View>
+            </View>
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 20,
+                backgroundColor: "#E0E0E0",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "row",
+                marginTop: "auto",
+                marginLeft: 20,
+                marginBottom: 20,
+              }}
+            >
+              <AntDesign name="hearto" size={24} color="black" />
+            </View>
+          </ImageBackground>
         ))}
       </ScrollView>
+      <View style={{ padding: 10 }}>
+        <Text style={{ fontSize: 15, fontWeight: "500" }}>
+          {route?.params?.title}
+        </Text>
+        <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 6 }}>
+          ₹{route.params.price}
+        </Text>
+      </View>
+      <Text style={{ height: 1, borderColor: "#D0D0D0", borderWidth: 1 }} />
+      <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
+        <Text>Color: </Text>
+        <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+          {route?.params?.color}{" "}
+        </Text>
+      </View>
+      <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
+        <Text>Size: </Text>
+        <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+          {route?.params?.size}
+        </Text>
+      </View>
+      <Text style={{ height: 1, borderColor: "#D0D0D0", borderWidth: 1 }} />
     </ScrollView>
   );
 };
